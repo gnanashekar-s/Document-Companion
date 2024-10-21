c5f1fe96de5622248dcacf5218310ffc7f6fe652
RAMKUMAR K <ramkumar.it22@bitsathy.ac.in>
2024-10-21 15:49:07 +0530

    minor error rectification

--- UI.py
+++ UI.py
@@ -91,7 +91,7 @@ if st.session_state["search_results"]:
                 if st.button("Summarize", key=f"summarize_{count}"):
                     # Check if the summary already exists in session_state
                     if file_path not in st.session_state["summaries"]:
-                        summary = summariser.summarize_file(st.session_state["pdf_texts"][file_path][1])
+                        summary = summariser.summarize_file(st.session_state["pdf_texts"][file_path][0])
                         st.session_state["summaries"][file_path] = summary
                     else:
                         summary = st.session_state["summaries"][file_path]
@@ -111,7 +111,10 @@ if st.session_state["search_results"]:
                         # Create a unique path for the audio file in a temporary directory
                         audio_file_path = os.path.join('temp_audio', f"summary_audio_{count}.wav")
                         # Generate audio for the summarized content
-                        audio(dict_to_string(st.session_state["summaries"][file_path]), audio_file_path)
+                        if type(st.session_state["summaries"][file_path])==dict:
+                            audio(dict_to_string(st.session_state["summaries"][file_path]), audio_file_path)
+                        elif type(st.session_state["summaries"][file_path]) == str:
+                            audio(st.session_state["summaries"][file_path], audio_file_path)
                         # Store the audio path in session_state
                         st.session_state["audio_paths"][file_path] = audio_file_path
 
